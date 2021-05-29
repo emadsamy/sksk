fefbc961add3f0e661849ee8cc1a3bfa34924d0d
A <msomslion@gmail.com>
2021-05-29 23:31:44 +0300

    ccc

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -12,20 +12,20 @@
 
   <!-- Links -->
   <!-- Project Shortcut Icon -->
-  <link rel="shortcut icon" href="{{ url('assets/img/logo.svg') }}" type="image/x-icon" />
+  <link rel="shortcut icon" href="{{ secure_asset('assets/img/logo.svg') }}" type="image/x-icon" />
   <link rel="icon" href="{{ url('assets/img/logo.svg') }}" type="image/x-icon" />
 
   <!-- Bootstrap V 4.1.3 -->
-  <link rel="stylesheet" href="{{ url('assets/css/bootstrap.css') }}" />
+  <link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap.css') }}" />
   <!-- FontAwesome 5 -->
-  <link rel="stylesheet" href="{{ url('assets/css/fontawesome.css') }}" />
+  <link rel="stylesheet" href="{{ secure_asset('assets/css/fontawesome.css') }}" />
   <!-- Icomoon Library -->
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
   <!-- Convert Images Svg To Icons -->
-  <link rel="stylesheet" href="{{ url('assets/css/icomoon.css') }}" />
+  <link rel="stylesheet" href="{{ secure_asset('assets/css/icomoon.css') }}" />
   <!-- Main Style -->
-  <link rel="stylesheet" href="{{ url('assets/css/style.css') }}" />
+  <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}" />
   <!-- Animate -->
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"> -->
   <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
