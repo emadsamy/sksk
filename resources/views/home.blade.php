b77056a45fba982c9776e206f7899188cec650f4
A <msomslion@gmail.com>
2021-06-01 11:02:14 +0300

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,7 +60,8 @@
             </button>
           </div>
           <video id="videoPlayer">
-            <source src="https://cdn.emaarmisr.com/wp-content/uploads/2021/04/Belle-Vie-TEXT-TOP.mp4" type="video/mp4">
+          
+            <source src="https://player.vimeo.com/video/557479190" type="video/mp4">
             Your browser does not support the video tag.
           </video>
         </div>
